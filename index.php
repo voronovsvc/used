b4eae5c1af27276897ea01f8571ba7c680cf8e80
Dmitry Chubar <afi13yandex.ru>
2015-05-14 19:26:31 +0300

    Ch

--- index.php
+++ index.php
@@ -9,24 +9,8 @@
 		print 'Catalog page!';
 		break;
 	  case '/auth':
-		print 'Страница автризации!';
+		print 'РђРІС‚РѕСЂРёР·Р°С†РёСЏ!';
 		break;
 	}
 	
-//	или
-	
-	echo "<br />";
-	echo "<br />";
-	
-	$page = 'auth';
-	
-	if ($_SERVER['REQUEST_URI'] === "/".$page)
-	echo 'Страница: '.$page;
-	
-	
-	
-	
-//	echo $_SERVER['REQUEST_URI'];
-	print_r ("<pre>");
-	echo $_SERVER['SERVER_NAME'];
-	print_r ("</pre>");
+	 // РљРѕРґРёСЂРѕРІРєР°
