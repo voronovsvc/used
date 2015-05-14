795c0c6641eec3343e3cfeafc71c43bc2393ca27
voronovsvc <voronovsvc@ya.ry>
2015-05-14 19:12:58 +0400

    first commit

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <?php
-switch ($_SERVER['REQUEST_URI']) {
-  case '/':
-    print 'Homepage!';
-    break;
-
-  case '/catalog':
-    print 'Catalog page!';
-    break;
-}
-
-
+	
+	switch ($_SERVER['REQUEST_URI']) {
+	  case '/':
+		print 'Homepage!';
+		break;
+	
+	  case '/catalog':
+		print 'Catalog page!';
+		break;
+	  case '/auth':
+		print 'Страница автризации!';
+		break;
+	}
+	
+//	или
+	
+	echo "<br />";
+	echo "<br />";
+	
+	$page = 'auth';
+	
+	if ($_SERVER['REQUEST_URI'] === "/".$page)
+	echo 'Страница: '.$page;
+	
+	
+	
+	
+//	echo $_SERVER['REQUEST_URI'];
+	print_r ("<pre>");
+	echo $_SERVER['SERVER_NAME'];
+	print_r ("</pre>");
