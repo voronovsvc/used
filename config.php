096c643ddefb22c3b9ae4387eef354728ad17ed1
Dmitry <dimoncds@gmail.com>
2015-06-21 10:48:27 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 ﻿<?php
 
 	define('DB_SERV', 'localhost');
-	define('DB_NAME', 'vh55536_used');
-	define('DB_USER', 'vh55536_voronov');
-	define('DB_PASS', 'svcvoronov11');
+	define('DB_NAME', '');
+	define('DB_USER', '');
+	define('DB_PASS', '');
 	
 ?>
